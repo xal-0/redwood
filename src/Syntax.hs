b575eec31a91f9b40362a6a07cada344d66c2700
Sam Schweigel <s.schweigel@gmail.com>
2021-02-24 17:26:31 -0800

    Comment Syntax.hs

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -3,19 +3,39 @@ module Syntax where
 type Program = Block
 
 type Ident = String
+
+-- | A list of statements to be executed one after the other, say, as
+-- the body of a function or a while loop.  Typically the value
+-- resulting from evaluating this is the value that results from
+-- evaluating the final statement.
 type Block = [Stmt]
 
-data Stmt = StmtExpr Expr
-          | StmtAssign Ident Expr
-          | StmtReturn (Maybe Expr)
-  deriving Show
+-- | Statements make up function bodies.  They evaluate to a value,
+-- but this is null in the case of assignments.  Expressions are
+-- automatically statements, but statements cannot be used in
+-- expressions.
+data Stmt
+  = StmtExpr Expr
+  | StmtAssign Ident Expr
+  | StmtReturn (Maybe Expr)
+  deriving (Show)
 
+-- | Binary operations, like arithmetic.
 data Binop = BinopPlus
-  deriving Show
+  deriving (Show)
 
-data Expr = ExprVariable Ident
-          | ExprNumber Double
-          | ExprBinop Binop Expr Expr
-          | ExprCall Expr [Expr]
-          | ExprFunc [Ident] Block
-  deriving Show
+-- | Expressions, which evaluate to a value and can have side effects.
+data Expr
+  = -- | A reference to a variable in the current scope.
+    ExprVariable Ident
+  | -- | A literal number.
+    ExprNumber Double
+  | -- | A binary operation (arithmetic).
+    ExprBinop Binop Expr Expr
+  | -- | Call a function (really just an expression that evaluates to
+    -- a closure) with some arguments.
+    ExprCall Expr [Expr]
+  | -- | A lambda: the ident list are the formal parameters, and the
+    -- block is the function body.
+    ExprFunc [Ident] Block
+  deriving (Show)
