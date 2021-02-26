2e58de6e5b1aa6eb966da4df865de9b75b1e91ff
Sam Schweigel <s.schweigel@gmail.com>
2021-02-25 21:09:31 -0800

    Make main use the new interpreter interface

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,6 @@
-module Graphics where
+module Main where
 
 import Interpreter
-import Parser
-import Graphics
-import System.IO
-import Text.Megaparsec
-import Text.Megaparsec.Char
 import System.Environment
 
 main :: IO ()
@@ -13,13 +8,7 @@ main = do
   args <- getArgs
   case args of
     [filename] -> do
-      fileContents <- readFile filename
-      case parse (stmts <* eof) filename fileContents of
-        Left err -> putStrLn (errorBundlePretty err)
-        Right prog -> do
-          print prog
-          result <- testInterpret prog
-          case result of
-            Left e -> print e
-            Right _ -> pure ()
+      interp <- makeInterpreter []
+      _ <- evalSource interp filename
+      pure ()
     _ -> putStrLn "provide the file to execute as an argument"
